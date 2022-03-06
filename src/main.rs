db19497cd7a61b307c4504cec4ee7e946b8cdebf
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-03-07 00:01:29 +0800

    第五天

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+extern crate core;
+
 mod base;
 
+use core::panicking::panic;
 use crate::base::*;
 
+#[derive(Debug)]
+struct MyType {
+    a: i64,
+    b: i32,
+}
+
 fn main() {
-    multi_type()
+    let t1 = MyType{
+        a: 1,
+        b: 2,
+    };
+    println!("{:p}", &t1.a);
+    println!("{:p}", &t1.b);
+    let t2 = t1;
+    println!("{:p}", &t2.a);
+    println!("{:p}", &t2.b)
+}
+
+fn test() -> ! {
+    panic("aaa");
 }
