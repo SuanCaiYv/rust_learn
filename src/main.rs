175a9b03840b906f10544473a01b27caa8e387de
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-05-16 16:08:22 +0800

    7 day

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
-extern crate core;
+struct Solution {}
 
-mod base;
-
-use crate::base::*;
-
-fn main() {
-    match error_result(false) {
-        Ok(str) => {
-            println!("{}", str)
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        if strs.len() == 0 {
+            return String::new();
         }
-        Err(err) => {
-            println!("err: {}", err)
+        let mut min_len = strs[0].len();
+        for i in 1..strs.len() {
+            min_len = min_len.min(strs[i].len());
         }
+        if min_len == 0 {
+            return String::new();
+        }
+        let mut idx: i32 = -1;
+        for i in 0..min_len {
+            let mut flag = true;
+            for j in 1..strs.len() {
+                if strs[0].bytes().nth(i).unwrap() != strs[j].bytes().nth(i).unwrap() {
+                    flag = false;
+                    break;
+                }
+            }
+            if !flag {
+                break;
+            }
+            idx = i as i32;
+        }
+        strs[0][0..(idx+1) as usize].to_string()
     }
 }
 
-pub fn error_result(flag: bool) -> Result<String, String> {
-    if flag {
-        Ok(String::from("success"))
-    } else {
-        Err(String::from("failed"))
-    }
+trait Test {}
+
+#[derive(Debug)]
+struct Type {}
+
+impl Test for Type {}
+
+fn main() {
+    let v = f();
+    print_type_of(&v);
+    println!("{}", Solution::longest_common_prefix(vec!["a".to_string(), "".to_string()]));
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
+}
+
+fn f() -> impl Test {
+    Type {}
 }
