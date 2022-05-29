227f1c6c0a51295a94d6bb3cac7ce92dc5c1a888
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-05-30 00:59:13 +0800

    day 11

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 extern crate core;
 
-use std::cell::{Cell, RefCell};
-use std::rc::Rc;
-
 mod base;
 
-fn main() {
-    let v = 0;
-    let r = RefCell::new(v);
-    let rc1 = Rc::new(r);
-    let rc2 = rc1.clone();
-    let rc3 = rc1.clone();
-    *rc2.borrow_mut() += 1;
-    *rc3.borrow_mut() += 1;
-    println!("{}", rc1.borrow());
+struct MyType<T> {
+    val: T
 }
+
+fn main() {
+    let mut x = MyType{val: String::from("aaa")};
+    let mut y = &mut x;
+    y.val = String::from("bbb")
+}
+
