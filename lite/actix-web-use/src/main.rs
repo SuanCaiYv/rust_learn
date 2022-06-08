427214fbd210500347ebe1d10089580538450f0a
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-06-09 00:36:05 +0800

    actix-web尝试

--- lite/actix-web-use/src/main.rs
+++ lite/actix-web-use/src/main.rs
@@ -1,33 +1,74 @@
-use std::sync::Mutex;
-use actix_web::{App, get, HttpResponse, HttpServer, Responder, web};
+use actix_web::{App, get, post, HttpResponse, HttpServer, Responder, route, web, guard};
 
-#[get("/t1")]
-async fn get_request() -> impl Responder {
+async fn get1() -> impl Responder {
+    HttpResponse::Ok().body("get1")
+}
+
+async fn post1() -> impl Responder {
+    HttpResponse::Ok().body("post1")
+}
+
+async fn get2() -> impl Responder {
+    HttpResponse::Ok().body("get2")
+}
+
+async fn post2() -> impl Responder {
+    HttpResponse::Ok().body("post2")
+}
+
+#[get("/ok")]
+async fn ok() -> impl Responder {
     HttpResponse::Ok().body("ok")
 }
 
-async fn post_request(body: String) -> impl Responder {
-    println!("{}", body);
-    HttpResponse::Ok().body("ok")
+fn config1(config: &mut web::ServiceConfig) {
+    config.service(
+        web::resource("/t2")
+            .route(web::get().to(|| async {
+                HttpResponse::Ok().body("get2")
+            }))
+            // 很明显，这里可以做权限拦截使用，限制请求的方法，或者解析Token等操作
+            .route(web::to(|| async {
+                HttpResponse::Ok().body("not allowed")
+            }))
+    );
 }
 
-#[get("/t2")]
-async fn app_data_test(data: web::Data<Mutex<i32>>) -> impl Responder {
-    let mut counter = data.lock().unwrap();
-    *counter += 1;
-    println!("counter is: {}", counter);
-    HttpResponse::Ok().body("ok")
+fn config2(config: &mut web::ServiceConfig) {
+    config.service(
+        web::resource("/t3")
+            .route(web::post().to(|| async {
+                HttpResponse::Ok().body("get3")
+            }))
+            .route(web::to(|| async {
+                HttpResponse::Ok().body("not allowed")
+            }))
+    );
 }
 
+/// 测试链接：
+/// get@/t1
+/// get@/t2
+/// get@/a
+/// !get@/t2
+/// get@/ok
+/// post@/b/t3
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(Mutex::new(0));
     HttpServer::new(move || {
         App::new()
-            .service(get_request)
-            .route("/t1", web::post().to(post_request))
-            .app_data(counter.clone())
-            .service(app_data_test)
+            .service(
+                web::scope("/t1")
+                    // 要求请求头必须包含如下
+                    .guard(guard::Header("Token", "code-with-buff"))
+                    .route("", web::get().to(|| async {HttpResponse::Ok().body("ok")}))
+            )
+            .configure(config1)
+            // 产生的请求不会影响到后续的操作，即config不会影响后面的请求路径和方法，是独立的
+            .route("/a", web::get().to(|| async {HttpResponse::Ok().body("ok")}))
+            // 这里的url会拼接前面的前导url
+            .service(web::scope("/b").configure(config2))
+            .service(ok)
     })
         .bind("127.0.0.1:8190")?
         .run()
