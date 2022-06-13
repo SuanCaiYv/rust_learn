492e051e41ba250ddf76d6f47a54bc2e1b75db62
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-06-13 23:09:17 +0800

    actix-web的使用

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,25 +1,30 @@
-use std::cell::{Cell, RefCell};
-use std::rc::Rc;
+#[derive(Debug)]
+struct MyType {
+    a: i64,
+    b: Box<Inner>,
+}
+
+#[derive(Debug)]
+struct Inner {
+    a: i32,
+    b: i32,
+}
 
 fn main() {
-    let a = Cell::new(1);
-    let b = RefCell::new(1);
-
-    let a_rc = Rc::new(a);
-    let b_rc = Rc::new(b);
-
-    let a_rc1 = a_rc.clone();
-    let a_rc2 = a_rc.clone();
-
-    let b_rc1 = b_rc.clone();
-    let b_rc2 = b_rc.clone();
-
-    a_rc1.set(a_rc1.get() + 1);
-    a_rc2.set(a_rc2.get() + 1);
-
-    *b_rc1.borrow_mut() += 1;
-    *b_rc2.borrow_mut() += 1;
-
-    println!("{}", a_rc.get()); // 3
-    println!("{}", *b_rc.borrow()); // 3
+    let mut t = MyType {
+        a: 1,
+        b: Box::new(Inner {
+            a: 2,
+            b: 3,
+        }),
+    };
+    let a;
+    {
+        a = &t;
+    }
+    let b;
+    {
+        b = &mut t;
+    }
+    println!("{:?}", a);
 }
--- lite/actix-web-use/src/main.rs
+++ lite/actix-web-use/src/main.rs
@@ -1,44 +1,27 @@
-use std::cell::Cell;
-use std::sync::Arc;
-use std::sync::atomic::{AtomicI32, Ordering};
-use std::thread;
-use std::time::Duration;
-use actix_web::{get, post, App, HttpRequest, HttpServer, web, HttpResponse};
-use serde::Deserialize;
+use actix_web::{get, HttpServer, App};
+use actix_web::middleware::{DefaultHeaders, Logger};
+use env_logger::Env;
 
-#[derive(Clone)]
-struct Counter {
-    local: Cell<i32>,
-    global: Arc<AtomicI32>,
+#[get("/t1")]
+async fn f() -> String {
+    "ok".to_string()
 }
 
-/// 请求链接：
+/// 请求连接
+/// post@/t1
+/// post@/t2
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = Counter {
-        // 实现内部可变性，但是因为每次clone都会得到一个新的Cell，所以线程之间是不同的Cell
-        local: Cell::new(0),
-        // AtomicI32会被多个线程共享，因为对Arc进行clone，得到的Arc彼此之间是共享底层数据的，即共享AtomicI32
-        global: Arc::new(AtomicI32::new(0))
-    };
-    HttpServer::new(move || {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+    HttpServer::new(|| {
         App::new()
-            .app_data(web::Data::new(counter.clone()))
-            .route("/t1", web::get().to(
-                |counter: web::Data<Counter>| async move {
-                    let thread_id = thread::current().id();
-                    counter.local.set(counter.local.get() + 1);
-                    let local = counter.local.get();
-                    counter.global.fetch_add(1, Ordering::Relaxed);
-                    let global = counter.global.load(Ordering::Relaxed);
-                    println!("{:?}: local: {}, global: {}", thread_id, local, global);
-                    // 故意拉慢处理速度，以让新的请求被新的线程处理
-                    thread::sleep(Duration::from_secs(1));
-                    "ok"
-                }
-            ))
+            .wrap(DefaultHeaders::new().add(("aaa", "bbb")))
+            .wrap(DefaultHeaders::new().add(("aaa", "aaa")))
+            .wrap(Logger::new("%a"))
+            .wrap(Logger::new("%t"))
+            .service(f)
     })
-        .bind("127.0.0.1:8190")?
+        .bind(("127.0.0.1", 8190))?
         .run()
         .await
 }
