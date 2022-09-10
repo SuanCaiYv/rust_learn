4aaea46ac9045c80da3f77c9a963c1f66735e92e
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-09-10 21:34:33 +0800

    异步

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,30 +1,58 @@
-#[derive(Debug)]
-struct MyType {
-    a: i64,
-    b: Box<Inner>,
-}
 
-#[derive(Debug)]
-struct Inner {
-    a: i32,
-    b: i32,
-}
-
+#![allow(unused)]
 fn main() {
-    let mut t = MyType {
-        a: 1,
-        b: Box::new(Inner {
-            a: 2,
-            b: 3,
-        }),
-    };
-    let a;
-    {
-        a = &t;
+    enum GeneratorState<Y, R> {
+        Yielded(Y),
+        Complete(R),
     }
-    let b;
-    {
-        b = &mut t;
+
+    trait Generator {
+        type Yield;
+        type Return;
+        fn resume(&mut self) -> GeneratorState<Self::Yield, Self::Return>;
     }
-    println!("{:?}", a);
+
+    enum GeneratorA<'a> {
+        Enter,
+        Yield1 {
+            to_borrow: String,
+            borrowed: &'a String, // uh, what lifetime should this have?
+        },
+        Exit,
+    }
+
+    impl GeneratorA<'_> {
+        fn start() -> Self {
+            GeneratorA::Enter
+        }
+    }
+
+    impl Generator for GeneratorA<'_> {
+        type Yield = usize;
+        type Return = ();
+        fn resume(&mut self) -> GeneratorState<Self::Yield, Self::Return> {
+            // lets us get ownership over current state
+            match std::mem::replace(self, GeneratorA::Exit) {
+                GeneratorA::Enter => {
+                    let to_borrow = String::from("Hello");
+                    let borrowed = &to_borrow; // <--- NB!
+                    let res = borrowed.len();
+
+                    let y = GeneratorA::Yield1 {to_borrow, borrowed};
+                    if let GeneratorA::Yield1 {ref to_borrow, mut borrowed} = y {
+                        borrowed = to_borrow;
+                    }
+                    *self = y;
+                    GeneratorState::Yielded(res)
+                }
+
+                GeneratorA::Yield1 {to_borrow, borrowed} => {
+                    println!("Hello {}", borrowed);
+                    *self = GeneratorA::Exit;
+                    GeneratorState::Complete(())
+                }
+                GeneratorA::Exit => panic!("Can't advance an exited generator!"),
+            }
+        }
+    }
 }
