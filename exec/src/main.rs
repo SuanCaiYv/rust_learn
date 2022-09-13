72429de0a2f601f2adeef8f2b6a1d7ec6d91d27d
SuanCaiYv <jokerblack2508826394@gmail.com>
2022-09-13 22:46:29 +0800

    异步

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,58 +1,64 @@
+#![feature(generator_trait)]
+#![feature(generators)]
 
-#![allow(unused)]
+use std::future::Future;
+use std::ops::{Generator, GeneratorState};
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::task::Poll::Pending;
+use std::time::Duration;
+use tokio::time::Instant;
+
+struct Timer {
+    delay_sec: u64,
+    when: Instant,
+}
+
+impl Timer {
+    fn new(delay_sec: u64) -> Self {
+        Timer {
+            delay_sec,
+            when: Instant::now() + Duration::from_secs(delay_sec),
+        }
+    }
+}
+
+impl Future for Timer {
+    type Output = String;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        return if Instant::now() <= self.when {
+            Poll::Ready(format!("run after {} seconds", self.delay_sec))
+        } else {
+            let waker = cx.waker().clone();
+            std::thread::spawn(move || {
+                std::thread::sleep(Duration::from_secs(self.delay_sec));
+                waker.wake();
+            });
+            Poll::Pending
+        }
+    }
+}
+
+struct Task {
+    generator: Option<dyn Generator<Yield=Pending<String>, Return=String>>
+}
+
+impl Task {
+    fn new() -> Task {
+        Task {
+            generator: None,
+        }
+    }
+}
+
+impl Future for Task {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Pending
+    }
+}
+
 fn main() {
-    enum GeneratorState<Y, R> {
-        Yielded(Y),
-        Complete(R),
-    }
-
-    trait Generator {
-        type Yield;
-        type Return;
-        fn resume(&mut self) -> GeneratorState<Self::Yield, Self::Return>;
-    }
-
-    enum GeneratorA<'a> {
-        Enter,
-        Yield1 {
-            to_borrow: String,
-            borrowed: &'a String, // uh, what lifetime should this have?
-        },
-        Exit,
-    }
-
-    impl GeneratorA<'_> {
-        fn start() -> Self {
-            GeneratorA::Enter
-        }
-    }
-
-    impl Generator for GeneratorA<'_> {
-        type Yield = usize;
-        type Return = ();
-        fn resume(&mut self) -> GeneratorState<Self::Yield, Self::Return> {
-            // lets us get ownership over current state
-            match std::mem::replace(self, GeneratorA::Exit) {
-                GeneratorA::Enter => {
-                    let to_borrow = String::from("Hello");
-                    let borrowed = &to_borrow; // <--- NB!
-                    let res = borrowed.len();
-
-                    let y = GeneratorA::Yield1 {to_borrow, borrowed};
-                    if let GeneratorA::Yield1 {ref to_borrow, mut borrowed} = y {
-                        borrowed = to_borrow;
-                    }
-                    *self = y;
-                    GeneratorState::Yielded(res)
-                }
-
-                GeneratorA::Yield1 {to_borrow, borrowed} => {
-                    println!("Hello {}", borrowed);
-                    *self = GeneratorA::Exit;
-                    GeneratorState::Complete(())
-                }
-                GeneratorA::Exit => panic!("Can't advance an exited generator!"),
-            }
-        }
-    }
 }
