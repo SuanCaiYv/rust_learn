e00ead36dacd56553c7f5e7bb6d9220ec120f2d2
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-02-07 00:25:44 +0800

    增加了Reference

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,19 +1,40 @@
 #![allow(unused)]
 
-fn main() {
-    // Default representation, alignment lowered to 2.
-    #[repr(packed(2))]
-    struct PackedStruct {
-        first: i16,
-        second: i8,
-        third: i32,
-    }
+trait T {
+    fn say(&self);
+}
 
-    // C representation, alignment raised to 8
-    #[repr(C, align(8))]
-    struct AlignedStruct {
-        first: i16,
-        second: i8,
-        third: i32,
+struct A {
+    a: i32,
+    b: i32,
+}
+
+struct B {
+    c: i64,
+    d: i64,
+}
+
+impl T for A {
+    fn say(&self) {
+        println!("A");
     }
+}
+
+impl T for B {
+    fn say(&self) {
+        println!("B");
+    }
+}
+
+fn main() {
+    let a = A { a: 1, b: 2 };
+    let b = B { c: 3, d: 4 };
+    // here, a and b are both trait object of T, but there are different types.
+    // so with the purpose of 'save' them to same format(or said, type), directly assignment is not allowed.
+    // we need an other way to resolve this, as you guess -- FatPointer is introduced to reach this goal.
+    // DST is just an noun to describe this kind of type that can not be stored in a single pointer(or reference).
+    let x: &dyn T = &a;
+    let y: &dyn T = &b;
+    x.say();
+    y.say();
 }
