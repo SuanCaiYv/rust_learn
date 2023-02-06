2f035d3b4c1ee947da7479c38f7128dd83b51841
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-02-06 18:02:00 +0800

    增加了Reference

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,7 +1,19 @@
 #![allow(unused)]
 
 fn main() {
-    fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
-        Box::new(|x| x + 1)
+    // Default representation, alignment lowered to 2.
+    #[repr(packed(2))]
+    struct PackedStruct {
+        first: i16,
+        second: i8,
+        third: i32,
+    }
+
+    // C representation, alignment raised to 8
+    #[repr(C, align(8))]
+    struct AlignedStruct {
+        first: i16,
+        second: i8,
+        third: i32,
     }
 }
