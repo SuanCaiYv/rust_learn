31ca07fb2d5f5d15d19303374cd946a66347268d
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-02-07 18:05:31 +0800

    增加了Reference

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,40 +1,21 @@
-#![allow(unused)]
+use std::fmt::Debug;
 
-trait T {
-    fn say(&self);
+trait DoSomething<T> {
+    fn do_sth(&self, value: T);
 }
 
-struct A {
-    a: i32,
-    b: i32,
-}
-
-struct B {
-    c: i64,
-    d: i64,
-}
-
-impl T for A {
-    fn say(&self) {
-        println!("A");
+impl<'a, T: Debug> DoSomething<T> for &'a usize {
+    fn do_sth(&self, value: T) {
+        println!("{:?}", value);
     }
 }
 
-impl T for B {
-    fn say(&self) {
-        println!("B");
-    }
+fn foo(b: Box<dyn for<'a> DoSomething<&'a usize>>) {
+    let s: usize = 10;
+    b.do_sth(&s);
 }
 
 fn main() {
-    let a = A { a: 1, b: 2 };
-    let b = B { c: 3, d: 4 };
-    // here, a and b are both trait object of T, but there are different types.
-    // so with the purpose of 'save' them to same format(or said, type), directly assignment is not allowed.
-    // we need an other way to resolve this, as you guess -- FatPointer is introduced to reach this goal.
-    // DST is just an noun to describe this kind of type that can not be stored in a single pointer(or reference).
-    let x: &dyn T = &a;
-    let y: &dyn T = &b;
-    x.say();
-    y.say();
+    let x = Box::new(&2usize);
+    foo(x);
 }
