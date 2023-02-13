556e4ecc3a577a80c8371e072c782ad3904f9c62
SuanCaiYv <jokerblack2508826394@gmail.com>
2023-02-13 17:42:22 +0800

    增加了Reference

--- exec/src/main.rs
+++ exec/src/main.rs
@@ -1,21 +1,25 @@
-use std::fmt::Debug;
-
-trait DoSomething<T> {
-    fn do_sth(&self, value: T);
-}
-
-impl<'a, T: Debug> DoSomething<T> for &'a usize {
-    fn do_sth(&self, value: T) {
-        println!("{:?}", value);
-    }
-}
-
-fn foo(b: Box<dyn for<'a> DoSomething<&'a usize>>) {
-    let s: usize = 10;
-    b.do_sth(&s);
+fn type_name<T: ?Sized>(_: &T) -> &'static str {
+    std::any::type_name::<T>()
 }
 
 fn main() {
-    let x = Box::new(&2usize);
-    foo(x);
+    let arr = [1, 2, 3, 4, 5, 6];
+    // error syntax, just for understanding
+    // let s1: [i32] = arr[0..2];
+    // let s2: [i32] = arr[2..6];
+    let ss1 = &arr[0..2];
+    let ss2 = &arr[3..6];
+    println!("{}", type_name(ss1));
+    println!("{}", type_name(ss));
+    println!("{}", type_name(&ss1));
+    println!("{}", type_name(&ss2));
+    /// output:
+    /// <br>
+    /// [i32]
+    /// <br>
+    /// [i32]
+    /// <br>
+    /// &[i32]
+    /// <br>
+    /// &[i32]
 }
